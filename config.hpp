bcd0797dd8f1c7e398284d6205d66256c90bed3f
kiko192 <163223426+kiko192@users.noreply.github.com>
2024-08-15 00:07:49 -0500

    The HYDE_PRIVATE_PRIVATE_STRING_XSMASH and HYDE_PRIVATE_STRING_SMASH macros were removed to simplify the logic.

--- config.hpp
+++ config.hpp
@@ -1,22 +1,19 @@
-
 #pragma once
 
 /**************************************************************************************************/
 
-#define HYDE_PRIVATE_STRING_XSMASH(X, Y) X##Y
-#define HYDE_PRIVATE_STRING_SMASH(X, Y) HYDE_PRIVATE_STRING_XSMASH(X, Y)()
-
-#define HYDE_PLATFORM_PRIVATE_APPLE() 0
-#define HYDE_PLATFORM_PRIVATE_MICROSOFT() 0
-
-#define HYDE_PLATFORM(X) HYDE_PRIVATE_STRING_SMASH(HYDE_PLATFORM_PRIVATE_, X)
+// Platform detection macros
+#define HYDE_PLATFORM_APPLE 0
+#define HYDE_PLATFORM_MICROSOFT 0
 
 #if defined(__APPLE__)
-    #undef HYDE_PLATFORM_PRIVATE_APPLE
-    #define HYDE_PLATFORM_PRIVATE_APPLE() 1
+    #undef HYDE_PLATFORM_APPLE
+    #define HYDE_PLATFORM_APPLE 1
 #elif defined(_MSC_VER)
-    #undef HYDE_PLATFORM_PRIVATE_MICROSOFT
-    #define HYDE_PLATFORM_PRIVATE_MICROSOFT() 1
+    #undef HYDE_PLATFORM_MICROSOFT
+    #define HYDE_PLATFORM_MICROSOFT 1
 #endif
 
+// Generic macro to check platform
+#define HYDE_PLATFORM_IS(X) (HYDE_PLATFORM_##X)
 /**************************************************************************************************/
